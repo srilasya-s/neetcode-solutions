26d638acbd39f8a59d4b764c09af6cd3259be873
Sri Lasya Siripurapu <srilasyaw@gmail.com>
2026-06-16 13:29:13 -0400

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        #creating an emptty dictionary
+        count = {}
+        res = 0
+        #considering 2 pointers l,r l at 0 and r will iterate
+        l=0
+
+        for r in range(len(s)):
+            # count of the element in the dictionary increases with 1 (key:value :: element : count )
+            count[s[r]]= 1 + count.get(s[r],0)
+            # this is the key
+            # the elements can be replaced as far as we have chance (k chances)
+            #no need to replace chars, just updating length is required
+            while (r-l+1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l+=1
+            res = max(res, r-l+1)
+        return res
+
+
+
+            
